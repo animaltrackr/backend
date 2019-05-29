0d000da62461f820696ba22c27a7269af8b4f30c
Brendon Earl <bearl@uvic.ca>
2019-05-29 16:32:15 -0700

    Ran black on source

--- report/models.py
+++ report/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from enum import Enum
+
 
 class TrackerStatus(Enum):
     A = "Active"
@@ -6,18 +8,21 @@ class TrackerStatus(Enum):
     L = "Lost"
     U = "Unresponsive"
 
+
 # Create your models here.
 class Tracker(models):
     animal_id: models.CharField(max_length=255)
     status: models.CharField(
-        max_length=3,
-        choices=[(tag, tag.value) for tag in TrackerStatus]
+        max_length=3, choices=[(tag, tag.value) for tag in TrackerStatus]
     )
+
     def __str__(self):
         return self.animal_id
 
+
 class Record(models):
     tracker = models.ForeignKey(Tracker, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField('date time recorded')
+    timestamp = models.DateTimeField("date time recorded")
+
     def __str__(self):
         return f"{self.tracker.animal_id} - {self.timestamp}"
