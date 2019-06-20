25dea483d51816aa9b4ebdaada508fe169563a46
Brendon Earl <bearl@uvic.ca>
2019-06-20 11:08:49 -0700

    Fix typo bugs 🤦‍♂️

--- report/models.py
+++ report/models.py
@@ -12,8 +12,8 @@ class TrackerStatus(Enum):
 
 # Create your models here.
 class Tracker(models.Model):
-    animal_id: models.CharField(max_length=255)
-    status: models.CharField(
+    animal_id = models.CharField(max_length=255, unique=True)
+    status = models.CharField(
         max_length=3, choices=[(tag, tag.value) for tag in TrackerStatus]
     )
 
@@ -22,10 +22,10 @@ class Tracker(models.Model):
 
 
 class Record(models.Model):
-    tracker: models.ForeignKey(Tracker, on_delete=models.CASCADE)
-    timestamp: models.DateTimeField("date time recorded")
-    geo_lat: models.DecimalField(max_digits=9, decimal_places=6)
-    geo_long: models.DecimalField(max_digits=9, decimal_places=6)
+    tracker = models.ForeignKey(Tracker, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField("date time recorded")
+    geo_lat = models.DecimalField(max_digits=9, decimal_places=6)
+    geo_long = models.DecimalField(max_digits=9, decimal_places=6)
 
     def __str__(self):
         return f"{self.tracker.animal_id} - {self.timestamp}"
