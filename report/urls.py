e4b43e79f055e04f40d88df179a12b9ead2437d2
Brendon Earl <bearl@uvic.ca>
2019-06-24 16:03:26 -0700

    Add template urls and views for report

--- report/urls.py
+++ report/urls.py
@@ -2,4 +2,19 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    # ex: /report/
+    path("", views.index, name="index"),
+    # ex: /report/trackers
+    path("trackers", views.trackers, name="trackers"),
+    # ex: /report/trackers/add
+    path("trackers/add", views.tracker_add, name="tracker add"),
+    # ex: /report/trackers/<uuid>
+    path("trackers/<uuid:tracker_id>", views.tracker_details, name="tracker details"),
+    # ex: /report/records
+    path("records", views.records, name="records"),
+    # ex: /report/records/add
+    path("records/add", views.record_add, name="record add"),
+    # ex: /report/records/<uuid>
+    path("records/<uuid:tracker_id>", views.record_details, name="record details"),
+]
--- report/views.py
+++ report/views.py
@@ -4,3 +4,29 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the report index.")
+
+
+def trackers(request):
+    return HttpResponse("You're looking at the list of trackers.")
+
+
+def tracker_add(request):
+    return HttpResponse("You're looking to add a tracker.")
+
+
+def tracker_details(request, tracker_id):
+    response = "You're looking at the details of tracker %s"
+    return HttpResponse(response % tracker_id)
+
+
+def records(request):
+    return HttpResponse("You're looking at the list of trackers.")
+
+
+def record_add(request):
+    return HttpResponse("You're looking to add a record")
+
+
+def record_details(request, record_id):
+    response = "You're looking at the details of record %s"
+    return HttpResponse(response % record_id)
