78c13da8535311cbb1bbc51b0417fcc5a57ce6d0
Brendon Earl <bearl@uvic.ca>
2019-07-03 20:35:00 -0700

    Rebuild top level routes

--- trackr_server/urls.py
+++ trackr_server/urls.py
@@ -14,6 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from rest_framework import routers
 
-urlpatterns = [path("admin/", admin.site.urls), path("report/", include("report.urls"))]
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    re_path(r"^", include(router.urls)),
+    re_path(r"^api-auth/", include("rest_framework.urls")),
+    path("admin/", admin.site.urls),
+    path("report/", include("report.urls")),
+]
