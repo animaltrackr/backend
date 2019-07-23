0000bd88a8ef0d25dd4a163d442a53af8664866b
Brendon Earl <bearl@uvic.ca>
2019-07-22 20:12:39 -0700

    troubleshoot setup.sh

--- .circleci/stage.sh
+++ .circleci/stage.sh
@@ -4,10 +4,11 @@ scp -r -v -o "StrictHostKeyChecking no" ~/repo ${DEPLOY_USER}@${DEPLOY_LOCATION}
 
 echo "***** SSHing into ec2 instance for setup *****\n"
 ssh -tt -o "StrictHostKeyChecking no" ${DEPLOY_USER}@${DEPLOY_LOCATION} << HERE
-    source aws-tags.sh
     killall screen
     rm -rf ~/api
     mv ~/tmp ~/api
+    source ./api/.circleci/aws-tags.sh
+    ls -la
     cd api
     pip3 install -r requirements.txt --user # <- I know this isn't ideal. Andy: Changed to --user to fix psycopg2.
     python3 ./manage.py migrate
